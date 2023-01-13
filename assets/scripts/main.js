6f808122978d77397d31ade6674b52b9cce715cc
DiegoDeLaCruz <u20201c334@upc.edu.pe>
2023-01-13 16:21:25 -0500

    added new feature

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,24 +7,51 @@ const score = document.getElementById('score-number'),
 
 // Number to Guess
 
-const guessNumber = Math.floor(Math.random() * 20 + 1)
+let guessNumber = Math.floor(Math.random() * 20 + 1)
 
 let scoreBase = 0
+let guessedNumbers = 0
+
+function updateScore() {
+    scoreBase += 5
+    guessedNumbers++
+    score.textContent = scoreBase.toString()
+}
+
+function updateGuessNumber() {
+    guessNumber = Math.floor(Math.random() * 20 + 1)
+}
+
+function displayMessage(text) {
+    message.textContent = text
+}
+
+function showGuessNumber() {
+    document.querySelector('.guess').classList.add('correct')
+    document.querySelector('.guess').textContent = guessNumber.toString()
+}
+
+function hideGuessNumber() {
+    document.querySelector('.guess').classList.remove('correct')
+    document.querySelector('.guess').textContent = "?"
+    displayMessage('Start guessing')
+}
 
 checkButton.addEventListener('click', () => {
 
     const checkNumber = Number(checkInput.value)
 
-    // When player wins
     if(checkNumber === guessNumber) {
-        message.textContent = 'Correct Number'
-        scoreBase += 5
-        score.textContent = scoreBase.toString()
+        displayMessage('Correct Number')
+        updateScore()
+        showGuessNumber()
+        setTimeout(hideGuessNumber,2000)
+        updateGuessNumber()
     }
     else if(checkNumber > guessNumber) {
-        message.textContent = 'Too high'
+        displayMessage('Too high')
     }
     else {
-        message.textContent = 'Too low'
+        displayMessage('Too low')
     }
 })
